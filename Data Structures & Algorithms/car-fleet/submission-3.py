ec6a7b199d1d3431793ac4cc10422bd63e809005
Aman Saini <159570384+Aman00240@users.noreply.github.com>
2026-09-24 17:51:59 +0530

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pair=[[p,s] for p,s in zip(position,speed)]
+
+        fleet_count=0
+        slowest_time_ahead=0.0
+
+        for p,s in sorted(pair)[::-1]:
+            time=(target-p)/s
+
+            if time>slowest_time_ahead:
+                fleet_count+=1
+                slowest_time_ahead=time
+            
+            
+        return fleet_count
